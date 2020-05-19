e353acb36ff27eb1bba24e425cf934ab5d1751e7
kiku ishimoto <kikuish@gmail.com>
2020-05-19 18:54:53 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,10 +1,11 @@
 class Backer
-  attr_reader :name
-  attr_accessor :backed_projects
+  attr_reader :name, :backed_projects
   def initialize(name)
     @name = name
     @backed_projects = []
   end
   def back_project(project)
+    @backed_projects << project
+    
   end
 end
